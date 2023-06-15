3285e05944f51cfcd4e77d4f6d923249a1414a93
Anthony <ndegwaanthony300@gmail.com>
2023-06-15 09:05:50 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/* 
+ * This program print a statement using puts 
+ * return:0 (success) 
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle"\n);
+return 0;
+}
