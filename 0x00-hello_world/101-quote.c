9b9a45022a511b4daa1ea6d44254257af7a313d3
Anthony <ndegwaanthony300@gmail.com>
2023-06-15 14:19:30 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 /**
  * main - every c program should xecuted from the main
  * Description: this program display the line to the standaard error
- * Return: 1 (Success)
+ * Return: 1 (Error)
  */
 int main(void)
 {
