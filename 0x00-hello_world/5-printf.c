a6b88ee50b384fa3058837e6133e812d7c1f4879
Anthony <ndegwaanthony300@gmail.com>
2023-06-15 10:36:43 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * main: This program print a sentence 
- * return: 0 (success)
+ * main - Program start excetion at the main
+ * Description:  This program print a proper grammer though the outcome
+ * is a piece of art
+ * the programm return zero
+ * Return: 0 (success)
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
