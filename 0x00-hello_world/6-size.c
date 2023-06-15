3cc975fa2d1ae7caf0407aff2282c2b049215487
Anthony <ndegwaanthony300@gmail.com>
2023-06-15 11:01:13 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - every program start executing at the main
+ * Description: This program display the size of the data type
+ * Return: (0) (Success)
+ */
+int main(void)
+{
+printf("size of char:" sizeof(char) "\n");
+printf("size of int:" sizeof(int) "\n");
+printf("size of long int:" sizeof(long int) "\n");
+printf("size of long long int:" sizeof(long long int) "\n");
+printf("size of float:" sizeof(float) "\n");
+return (0);
+}
