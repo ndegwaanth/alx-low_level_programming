086ababe617a8a8c9824ddb24fca2662d4484989
Anthony <ndegwaanthony300@gmail.com>
2023-06-16 12:56:10 -0700

    ./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - program start executing from main
+ * Description: Display 0 - 10
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int a = 0;
+while (a < 10)
+{
+	putchar(a);
+	a++;
+}
+return (0);
+}
