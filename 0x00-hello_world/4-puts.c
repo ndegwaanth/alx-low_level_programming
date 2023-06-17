bec5f704d4005c897d88db2700578fc923a54a03
Anthony <ndegwaanthony300@gmail.com>
2023-06-17 03:13:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
+	puts("\n");
 	return (0);
 }
