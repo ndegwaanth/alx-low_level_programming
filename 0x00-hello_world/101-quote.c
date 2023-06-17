1b593deeb9b241801f6fdf2d124a8bdc7e229f53
Anthony <ndegwaanthony300@gmail.com>
2023-06-17 04:46:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,12 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/syscall.h>
-
+#include <stdio.h>
 /**
- * main - Entry point of the program
- *
- * Description: Prints a sentence to standard error using write system call.
- * Return: Always 1 (Error)
+ * main - start execution
+ * Description: program display a statement with a standart error
+ * Return: 1 (Error)
  */
 int main(void)
 {
-char *message = "and that piece of art's useful\" - Dora Korpar, 2015-10-19\n";
-size_t length = strlen(message);
-sycall(SYS_write, STDERR_FILENO, message, length);
+char *standard = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+fputs(standard, stdout);
 return (1);
 }
