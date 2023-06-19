d4de8ba7b42c9505f198c3ba537e35e38f94834a
Anthony <ndegwaanthony300@gmail.com>
2023-06-19 20:33:22 +0300

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,26 +10,20 @@ int m, n;
 
 for (m = 0; m < 100; m++)
 {
-	for (n = m; n < 100; n++)
+	for (n = 0; n < 100; n++)
 	{
-		int ten1 = m / 10;
-		int one1 = n % 10;
-		int ten2 = n / 10;
-		int one2 = m % 10;
-
-		putchar(ten1 / 10 + '0');
-		putchar(ten1 % 10 + '0');
-		putchar(one1 / 10 + '0');
-		putchar(one1 % 10 + '0');
-		putchar(' ');
-		putchar(ten2 / 10 + '0');
-		putchar(ten2 % 10 + '0');
-		putchar(one2 / 10 + '0');
-		putchar(one2 % 10 + '0');
-		if (m != 99 || n != 99)
+		if (m < n)
 		{
-			putchar(',');
+			putchar((m / 10) + 48);
+			putchar((m % 10) + 48);
 			putchar(' ');
+			putchar((n / 10) + 48);
+			putchar((m % 10) + 48);
+			if (m != 98 || n != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 }
