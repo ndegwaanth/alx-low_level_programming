6d947b7b31a790ddc851e69bc1b3edfcdbcb5ec6
Anthony <ndegwaanthony300@gmail.com>
2023-06-21 12:57:59 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,9 +24,9 @@ void print_to_98(int n)
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-				printf("%d ,", j);
+				printf(", %d", j);
 			else if (j == 98)
-				printf("%d ,", j);
+				printf(", %d", j);
 		}
 	}
 }
