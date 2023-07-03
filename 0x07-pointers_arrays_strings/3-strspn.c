f8c2ffcaf6dad688340a375083a0353a82d23be8
ndegwaanth <ndegwaanthony300@gmail.com>
2023-07-03 15:41:35 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix
+ * @s: this the string
+ * @accept: this represent the bytes
+ * Return: return the initial string s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int m;
+
+	while (*s)
+	{
+		for (m = 0; s[m]; m++)
+		{
+			if (*s == accept[m])
+			{
+				n++;
+				break;
+			}
+			else if (accept[m] == '\0')
+				return (n);
+		}
+		s++;
+	}
+}
