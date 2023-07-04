af5c5a4396fe42611df2e926ec62f98f4d4c4f64
ndegwaanth <ndegwaanthony300@gmail.com>
2023-07-04 12:42:13 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - sets the vall of pointer to char
+ * @s: dereference ponter
+ * @to: the character assigned to
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+ 
