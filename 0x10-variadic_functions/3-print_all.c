36f7571fb2831270bc89e5fa3497d0d9c814855f
ndegwaanth <ndegwaanthony300@gmail.com>
2023-07-20 09:34:34 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,46 +9,46 @@
  */
 void print_all(const char * const format, ...)
 {
-    unsigned int m;
-    int n;
-    char *string;
-    va_list items;
+	unsigned int m;
+	int n;
+	char *string;
+	va_list items;
 
-    va_start(items, format);
-    m = 0;
-    while (format != NULL && format[m] != '\0')
-    {
-        switch (format[m])
-        {
-            case 'c':
-                printf("%c", va_arg(items, int));
-                n = 0;
-                break;
-            case 'i':
-                printf("%d", va_arg(items, int));
-                n = 0;
-                break;
-            case 'f':
-                printf("%f", va_arg(items, double));
-                n = 0;
-                break;
-            case 's':
-                string = va_arg(items, char*);
-                if (string == NULL)
-                {
-                    string = "(nil)";
-                }
-                printf("%s", string);
-                n = 0;
-                break;
-            default:
-                n = 1;
-                break;
-        }
-        if (format[m + 1] != '\0' && n == 0)
-            printf(",");
-        m++;
-    }
-    printf("\n");
-    va_end(items);
+	va_start(items, format);
+	m = 0;
+	while (format != NULL && format[m] != '\0')
+	{
+		switch (format[m])
+		{
+			case 'c':
+				printf("%c", va_arg(items, int));
+				n = 0;
+				break;
+			case 'i':
+				printf("%d", va_arg(items, int));
+				n = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(items, double));
+				n = 0;
+				break;
+			case 's':
+				string = va_arg(items, char*);
+				if (string == NULL)
+				{
+					string = "(nil)";
+				}
+				printf("%s", string);
+				n = 0;
+				break;
+			default:
+				n = 1;
+				break;
+		}
+		if (format[m + 1] != '\0' && n == 0)
+			printf(",");
+		m++;
+	}
+	printf("\n");
+	va_end(items);
 }
