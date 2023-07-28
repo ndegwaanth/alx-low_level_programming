6daa1057b6b955d9916d4ed92e1ff44f09f8aaed
ndegwaanth <ndegwaanthony300@gmail.com>
2023-07-28 04:49:41 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,19 +6,23 @@
  */
 void print_diagonal(int n)
 {
-	int m, p;
-
 	if (n <= 0)
 	{
-		_putchar('n');
-	}
-	for (m = 0; m <= n; m++)
-	{
-		for (p = 0; p <= m; p++)
-		{
-			_putchar(' ');
-		}
-		_putchar('\\');
 		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
 	}
 }
