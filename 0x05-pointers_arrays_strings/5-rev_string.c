d9e451bca3ebf6d8c2d7978e5fbe4d714b68e11e
ndegwaanth <ndegwaanthony300@gmail.com>
2023-07-28 03:34:35 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - print a string in reverse
+ * @s: strig to print
+ * Return: void
+ */
+void rev_string(char *s)
+{
+        int length, size;
+	char string = s[0];
+
+        for (length = 0; s[length] != '\0';)
+        {
+		length++;
+        }   
+	for (size = 0; size < length; size++)
+        {
+		length--;
+		string = s[size];
+		s[size] = s[length];
+		s[length] = string;
+	}
+}
