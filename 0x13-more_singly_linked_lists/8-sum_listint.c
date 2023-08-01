757a1c266672249b3a2f1585640ef861b18fa32c
Anthony Ndegwa <ndegwaanthony300@gmail.com>
2023-08-01 23:19:58 +0300

    more singly linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - return the sum of all the data
+ * @head: the first element of a list
+ * Return: the sumvof all data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp != NULL)
+	{
+		sum = sum + temp->n;
+		temp = temp->next;
+		return (sum);
+	}
+
+	return (sum);
+}
