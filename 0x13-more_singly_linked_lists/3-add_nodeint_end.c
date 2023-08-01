6cbf24bf26abe66e3d8a2b8472ed35e8daeef3e5
ndegwaanth <ndegwaanthony300@gmail.com>
2023-08-01 13:40:33 -0700

    update the singly linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,15 +18,15 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	{
 		return (NULL);
 	}
-	
+
 	new_node->n = n;
 	new_node->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
-	}	
-	
+	}
+
 	while (temp->next)
 	{
 		temp = temp->next;
