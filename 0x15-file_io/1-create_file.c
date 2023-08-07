53cb747080195074d23578799339afb0859375dd
ndegwaanth <ndegwaanthony300@gmail.com>
2023-08-07 13:26:00 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,29 +9,28 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-    int file, f, m;
+	int file, f, m;
 
-    if (filename == NULL)
-    {
-        return (-1);
-    }
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 
-    if (text_content != NULL)
-    {
-        m = 0;
-        while(text_content[m])
-        {
-            m++;
-        }
+	if (text_content != NULL)
+	{
+		m = 0;
+		while (text_content[m])
+		{
+			m++;
+		}
+	}
 
-    }
-    file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    f = write(file, text_content, m);
-    
-    if (file == -1 || f == -1)
-    {
-        return (-1);
-    }
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	f = write(file, text_content, m);
+	if (file == -1 || f == -1)
+	{
+		return (-1);
+	}
 
-    return (1);
+	return (1);
 }
