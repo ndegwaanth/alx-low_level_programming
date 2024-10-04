5473755d3630e04fc2f63e7c69a2b1900c784d4a
Anthony <ndegwaanthony300@gmail.com>
2024-10-04 18:39:13 +0300

    updated 105-jump_list.c

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,36 +1,40 @@
 #include "search_algos.h"
 
 /**
- * jump_list - Searches for a value in a sorted singly linked list using Jump search
- * @list: Pointer to the head of the list
- * @size: The number of nodes in the list
- * @value: The value to search for
+ * jump_list - searches for a value in a sorted singly linked list of integers
+ *             using the Jump search algorithm
+ * @list: pointer to the head of the linked list
+ * @size: number of nodes in the list
+ * @value: value to search for
  *
- * Return: Pointer to the first node where value is located, or NULL if not found
+ * Return: pointer to the first node where value is located, or NULL if value
+ *         is not found or the list is NULL
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-    size_t jump_step = sqrt(size);
-    listint_t *prev = list;
-    listint_t *current = list;
-    
-    if (!list)
+    size_t jump_step, step;
+    listint_t *prev, *current;
+
+    if (!list || size == 0)
         return (NULL);
 
+    jump_step = sqrt(size);
+    prev = list;
+    current = list;
+
     /* Jump through the list */
-    while (current->next && current->index < size - 1)
+    while (current->next && current->index < size && current->n < value)
     {
         prev = current;
-        for (size_t i = 0; i < jump_step && current->next; i++)
+        for (step = 0; step < jump_step && current->next; step++)
             current = current->next;
 
         printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-        if (current->n >= value)
-            break;
     }
 
-    /* Perform linear search between prev and current */
     printf("Value found between indexes [%lu] and [%lu]\n", prev->index, current->index);
+
+    /* Perform a linear search in the sublist */
     while (prev && prev->index <= current->index)
     {
         printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
